2d998b5a4024399dcce9eaecd4e98ce1adaff5f6
garrymarkus <premsinghiitjee@gmail.com>
2026-02-18 10:01:33 +0530

    ML Resource added

--- genAI/python/10_oop/01_simple_class.py
+++ genAI/python/10_oop/01_simple_class.py
@@ -7,6 +7,8 @@ class ChaiTime:
 print(type(Chai))
 
 ginger_tea = Chai()
+standard_tea = Chai()
 print(type(ginger_tea))
+print(type(standard_tea))
 print(type(ginger_tea) is Chai)
 print(type(ginger_tea) is ChaiTime)
